2aca6374cdb97d1765ef37dcd53b44b659efe8e2
Alex Ainslie <alex.ainslie99@gmail.com>
2023-03-15 21:44:23 +0000

    Added NavBar basics

---
+++ src/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {AppBar,
+Container,
+ToolBar} from '@mui/material'
+
+const categories = ["Men's Clothing", "Women's Clothing", "Jewelery", "Electronics"];
+
+function NavBar(){
+
+    return(
+        <AppBar>
+            <Container>
+                <ToolBar>
+
+                </ToolBar>
+            </Container>
+        </AppBar>
+    )
+}

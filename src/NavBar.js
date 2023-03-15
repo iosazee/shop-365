1ba6eec6388cf357aa7f558c15daed2305b0122e
Alex Ainslie <alex.ainslie99@gmail.com>
2023-03-15 22:23:53 +0000

    Added Dropdown Menu for Categories

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,19 +1,62 @@
 import React from 'react';
 import {AppBar,
 Container,
-ToolBar} from '@mui/material'
+Toolbar,
+Box,
+Button,
+Menu,
+MenuItem,
+Typography} from '@mui/material'
 
 const categories = ["Men's Clothing", "Women's Clothing", "Jewelery", "Electronics"];
 
 function NavBar(){
 
+    const [categoryMenu, setCategoryMenu] = React.useState(null);
+
+    const handleOpenCategoryMenu = (event) => {
+        setCategoryMenu(event.currentTarget);
+    };
+
+    const handleCloseCategoryMenu = () => {
+        setCategoryMenu(null);
+      };
+
     return(
         <AppBar>
             <Container>
-                <ToolBar>
-
-                </ToolBar>
+                <Toolbar>
+                    <Box>
+                        <Button onClick={handleOpenCategoryMenu}
+                        sx={{ my: 2, color: 'white', display: 'block' }}>
+                            Categories
+                        </Button>
+                        <Menu
+                            sx={{ mt: '45px' }}
+                            anchorEl={categoryMenu}
+                            anchorOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left'
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(categoryMenu)}
+                            onClose={handleCloseCategoryMenu}
+                            >
+                            {categories.map((category) => (
+                                <MenuItem key={category} onClick={handleCloseCategoryMenu}>
+                                    <Typography textAlign="center">{category}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+                </Toolbar>
             </Container>
         </AppBar>
     )
 }
+
+export default NavBar;

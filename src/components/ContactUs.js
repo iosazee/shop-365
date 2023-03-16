237cad90ff3194739fb9c90e0fd86ccf66ba4b63
Zee Mudia <iosazee1@gmail.com>
2023-03-16 04:58:46 +0000

    update to contact form

--- src/components/ContactUs.js
+++ src/components/ContactUs.js
@@ -35,9 +35,16 @@ const ContactUs = () => {
         if(contactInfo.message.length <= 10) {
             setFormResponseMessage("Please enter in a valid message")
         } else {
-            setFormResponseMessage("You submitted the form successfully.")
+            setFormResponseMessage("Form submitted successfully");
+            setContactInfo({
+                firstName: "",
+                lastName: "",
+                email: "",
+                message: ""
+            })
+
         }
-    } 
+    }
 
     return (
         <section>

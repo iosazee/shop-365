397fe4900b628cec8e12d184396b3e25645db1a3
Alex Ainslie <alex.ainslie99@gmail.com>
2023-03-16 14:57:13 +0000

    Added Hamburger Nav menu

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {React, useState} from 'react';
 import {AppBar,
 Container,
 Toolbar,
@@ -8,13 +8,17 @@ Menu,
 MenuItem,
 Typography,
 IconButton} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import AccountIcon from '@mui/icons-material/AccountCircleRounded';
 
 const categories = ["Men's Clothing", "Women's Clothing", "Jewelery", "Electronics"];
+const pages =["Categories", "Contact Us", "Profile"];
 
 function NavBar(){
 
-    const [categoryMenu, setCategoryMenu] = React.useState(null);
+    // Category Dropdown
+    const [categoryMenu, setCategoryMenu] = useState(null);
 
     const handleOpenCategoryMenu = (event) => {
         setCategoryMenu(event.currentTarget);
@@ -24,11 +28,66 @@ function NavBar(){
         setCategoryMenu(null);
     };
 
+    // Hamburger Menu
+    const[anchorElNav, setAnchorElNav] = useState(null);
+
+    const handleOpenNavMenu = (event) => {
+        setAnchorElNav(event.currentTarget);
+    }
+
+    const handleCloseNavMenu = () => {
+        setAnchorElNav(null);
+    }
+
     return(
-        <AppBar>
+        <AppBar position="static">
             <Container>
                 <Toolbar disableGutters>
-                    <Box>
+                    <Typography
+                        variant="h6"
+                        sx={{display: { xs: 'none', md: 'flex' }}}
+                    >
+                        Logo
+                    </Typography>
+
+                    <Box sx={{flexGrow: 1, display:{xs:"flex", md:"none"}}}>
+                        <IconButton
+                            size="large"
+                            aria-label="account of current user"
+                            aria-controls="menu-appbar"
+                            aria-haspopup="true"
+                            onClick={handleOpenNavMenu}
+                            color="inherit"
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Menu
+                            id="menu-appbar"
+                            anchorEl={anchorElNav}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'left',
+                            }}
+                            keepMounted
+                            transformOrigin={{
+                                vertical: 'top',
+                                horizontal: 'left',
+                            }}
+                            open={Boolean(anchorElNav)}
+                            onClose={handleCloseNavMenu}
+                            sx={{
+                                display: { xs: 'block', md: 'none' },
+                            }}
+                        >
+                            {pages.map((page) => (
+                                <MenuItem key={page} onClick={handleCloseNavMenu}>
+                                    <Typography textAlign="center">{page}</Typography>
+                                </MenuItem>
+                            ))}
+                        </Menu>
+                    </Box>
+
+                    <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
                         <Button onClick={handleOpenCategoryMenu}
                         sx={{ my: 2, color: 'white', display: 'block' }}>
                             Categories
@@ -55,12 +114,11 @@ function NavBar(){
                             ))}
                         </Menu>
                     </Box>
-                    <Typography
-                        variant="h6"
-                        sx={{display: { xs: 'none', md: 'flex' }}}
-                    >
-                        Logo
-                    </Typography>
+
+                    <IconButton sx={{ flexGrow: 0, display: { xs: 'none', md: 'flex' } }}>
+                        <AccountIcon/>
+                    </IconButton>
+
                     <IconButton sx={{ flexGrow: 0 }}>
                         <ShoppingCartIcon />
                     </IconButton>

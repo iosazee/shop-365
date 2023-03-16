c4f6f3650fc064c525556c814c24bd5e6bc02a73
Zee Mudia <iosazee1@gmail.com>
2023-03-16 18:41:43 +0000

    added productdetail component

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,15 +1,19 @@
 import { Grid } from '@mui/material'
 import ProductCard from "./ProductCard"
+import { Container } from '@mui/material'
 
 const Products = ({products}) => {
   return (
-    <Grid container spacing={3}>
+    <Container  maxWidth={'lg'} sx={{my: 4}} >
+       <Grid container spacing={3}>
         {products.map(product => (
             <Grid key={product.id} item xs={12} sm={6} md={6} lg={4}>
                 <ProductCard product={product} />
             </Grid>
-        ))}        
+        ))}
     </Grid>
+    </Container>
+   
   )
 }
 

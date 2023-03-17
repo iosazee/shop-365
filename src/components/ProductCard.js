92792db6e239dc105d3f491f9aa8d1dfcf39b053
Zee Mudia <iosazee1@gmail.com>
2023-03-17 20:44:45 +0000

    added react lazyload library to lazy load the images and make the site more performant

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -6,25 +6,28 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import AddShoppingCartOutlinedIcon from '@mui/icons-material/AddShoppingCartOutlined'
 import { Link } from 'react-router-dom';
+import LazyLoad from 'react-lazy-load';
+
 
 const ProductCard = ({product}) => {
 
- 
 
   return (
     <Card sx={{boxShadow: 'none', border: '1px solid #ccc'}}>
-      <CardMedia
-        sx={{ height: 500 }}
-        image={product.image}
-        title={product.title}
-      />
+      <LazyLoad height={500} offset={200} >
+        <CardMedia
+          sx={{ height: 500 }}
+          image={product.image}
+          title={product.title}
+        />
+      </LazyLoad>
       <CardContent>
         <Typography align="center" component="p" sx={{ minHeight: 70 }}>
             {product.title}
         </Typography>
       </CardContent>
       <CardActions>
-        <Link to="/cart" ><Button sx={{ width: '50%'}} variant='outlined' size='small' startIcon={<AddShoppingCartOutlinedIcon />}> £{product.price.toFixed(2)}</Button> </Link>
+        <Button sx={{ width: '50%'}} variant='outlined' size='small' startIcon={<AddShoppingCartOutlinedIcon />}> £{product.price.toFixed(2)}</Button>
         <Link to={`/products/${product.id}`} style={{width:"50%", textDecoration:"none"}} >
           <Button sx={{width:"160px"}} variant='outlined'
             color='success' size='small'>Details

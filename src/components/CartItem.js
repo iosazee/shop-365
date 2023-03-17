4cd25fdfc83cc1eaaa17f902069f47cd71f99e26
Zee Mudia <iosazee1@gmail.com>
2023-03-17 22:14:28 +0000

    some minor corrections

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -6,7 +6,7 @@ const CartItem = ({itemData, deleteCartItem}) => {
 
             <CardMedia component="img" image={itemData.image} title={itemData.title}
              sx={{height:"100px", width:"80px"}} />
-            <Typography variant="h6" > {itemData.title} </Typography>
+            <Typography variant="p" > {itemData.title} </Typography>
             <Typography variant="p" > £ {itemData.price} </Typography>
             <Typography variant="p" > {itemData.count} </Typography>
             <Button size="small"

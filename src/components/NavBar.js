c368c89eacd7147e3d6ef7484d71fef158e2f7bd
Alex Ainslie <alex.ainslie99@gmail.com>
2023-03-17 14:53:35 +0000

    Added Accordion to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -16,6 +16,7 @@ import {AppBar,
 import MenuIcon from '@mui/icons-material/Menu';
 import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
 import AccountIcon from '@mui/icons-material/AccountCircleRounded';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 
 const categories = ["Men's Clothing", "Women's Clothing", "Jewelery", "Electronics"];
 
@@ -50,133 +51,152 @@ function NavBar(){
         setExpanded(newExpanded ? panel : false);
     };
 
+    // Search Accordion
+    const [searchExpand, setSearchExpand] = useState(false);
+    const toggleAccordion = () => {
+      setSearchExpand((prev) => !prev);
+    };
+
     return(
         <AppBar position="static">
-            <Container>
-                <Toolbar disableGutters>
-                <Grid container spacing={0}>
-                    <Box sx={{flexGrow: 1, display:{xs:"flex", md:"none"}}}>
-                        <IconButton
-                            size="large"
-                            aria-label="account of current user"
-                            aria-controls="menu-appbar"
-                            aria-haspopup="true"
-                            onClick={handleOpenNavMenu}
-                            color="inherit"
-                        >
-                            <MenuIcon />
-                        </IconButton>
+            <Accordion expanded={searchExpand}>
+                <AccordionSummary>
+                    <Container>
+                        <Toolbar disableGutters>
+                            <Grid container spacing={0}>
+                                <Grid item xs={4} sx={{display:'flex', alignItems:'center', justifyContent:'space-evenly'}}>
+                                    <Box sx={{flexGrow: 0, display:{xs:"flex", md:"none"}}}>
+                                        <IconButton
+                                            size="large"
+                                            aria-label="account of current user"
+                                            aria-controls="menu-appbar"
+                                            aria-haspopup="true"
+                                            onClick={handleOpenNavMenu}
+                                            color="inherit"
+                                        >
+                                            <MenuIcon />
+                                        </IconButton>
 
-                        <Menu
-                            id="menu-appbar"
-                            anchorEl={anchorElNav}
-                            anchorOrigin={{
-                                vertical: 'bottom',
-                                horizontal: 'left',
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'left',
-                            }}
-                            open={Boolean(anchorElNav)}
-                            onClose={handleCloseNavMenu}
-                            sx={{
-                                display: { xs: 'block', md: 'none' },
-                            }}
-                        >
-                            <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
-                                <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-                                    <Typography>Categories</Typography>
-                                </AccordionSummary>
-                                <AccordionDetails>
-                                    {categories.map((category) => (
-                                        <MenuItem key={category} onClick={handleCloseCategoryMenu}>
-                                            <Typography textAlign="center">{category}</Typography>
-                                        </MenuItem>
-                                    ))}
-                                </AccordionDetails>
-                            </Accordion> 
+                                        <Menu
+                                            id="menu-appbar"
+                                            anchorEl={anchorElNav}
+                                            anchorOrigin={{
+                                                vertical: 'bottom',
+                                                horizontal: 'left',
+                                            }}
+                                            keepMounted
+                                            transformOrigin={{
+                                                vertical: 'top',
+                                                horizontal: 'left',
+                                            }}
+                                            open={Boolean(anchorElNav)}
+                                            onClose={handleCloseNavMenu}
+                                            sx={{
+                                                display: { xs: 'block', md: 'none' },
+                                            }}
+                                        >
+                                            <Accordion expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
+                                                <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+                                                    <Typography>Categories</Typography>
+                                                </AccordionSummary>
+                                                <AccordionDetails>
+                                                    {categories.map((category) => (
+                                                        <MenuItem key={category} onClick={handleCloseCategoryMenu}>
+                                                            <Typography textAlign="center">{category}</Typography>
+                                                        </MenuItem>
+                                                    ))}
+                                                </AccordionDetails>
+                                            </Accordion> 
 
-                            <MenuItem key="contactUs" onClick={handleCloseNavMenu}>
-                                <Link to="/contact" style={{color: 'inherit', textDecoration: 'none' }}>
-                                    <Typography textAlign="center">Contact Us</Typography>
-                                </Link>
-                            </MenuItem>
-                            <MenuItem key="profile" onClick={handleCloseNavMenu}>
-                                <Link to="/profile" style={{color: 'inherit', textDecoration: 'none' }}>
-                                    <Typography textAlign="center">Profile</Typography>
-                                </Link>
-                            </MenuItem>
-                        </Menu>
-                    </Box>
-                    
-                    <Grid item md={4} sx={{display:'flex', alignItems:'center', justifyContent:'space-evenly'}}>
-                    <Box sx={{display: { xs: 'none', md: 'flex' } }}>
-                        <Button onClick={handleOpenCategoryMenu}
-                        sx={{ my: 2, color: 'white', display: 'block' }}>
-                            Categories
-                        </Button>
-                        <Menu
-                            sx={{ mt: '45px' }}
-                            anchorEl={categoryMenu}
-                            anchorOrigin={{
-                                vertical: 'top',
-                                horizontal: 'left'
-                            }}
-                            keepMounted
-                            transformOrigin={{
-                                vertical: 'top',
-                                horizontal: 'left',
-                            }}
-                            open={Boolean(categoryMenu)}
-                            onClose={handleCloseCategoryMenu}
-                        >
-                            {categories.map((category) => (
-                                <MenuItem key={category} onClick={handleCloseCategoryMenu}>
-                                    <Typography textAlign="center">{category}</Typography>
-                                </MenuItem>
-                            ))}
-                        </Menu>
-                    </Box>
+                                            <MenuItem key="contactUs" onClick={handleCloseNavMenu}>
+                                                <Link to="/contact" style={{color: 'inherit', textDecoration: 'none' }}>
+                                                    <Typography textAlign="center">Contact Us</Typography>
+                                                </Link>
+                                            </MenuItem>
+                                            <MenuItem key="profile" onClick={handleCloseNavMenu}>
+                                                <Link to="/profile" style={{color: 'inherit', textDecoration: 'none' }}>
+                                                    <Typography textAlign="center">Profile</Typography>
+                                                </Link>
+                                            </MenuItem>
+                                        </Menu>
+                                    </Box>
+                                
+                                    <Box sx={{display: { xs: 'none', md: 'flex' } }}>
+                                        <Button onClick={handleOpenCategoryMenu}
+                                        sx={{ my: 2, color: 'white', display: 'block' }}>
+                                            Categories
+                                        </Button>
+                                        <Menu
+                                            sx={{ mt: '45px' }}
+                                            anchorEl={categoryMenu}
+                                            anchorOrigin={{
+                                                vertical: 'top',
+                                                horizontal: 'left'
+                                            }}
+                                            keepMounted
+                                            transformOrigin={{
+                                                vertical: 'top',
+                                                horizontal: 'left',
+                                            }}
+                                            open={Boolean(categoryMenu)}
+                                            onClose={handleCloseCategoryMenu}
+                                        >
+                                            {categories.map((category) => (
+                                                <MenuItem key={category} onClick={handleCloseCategoryMenu}>
+                                                    <Typography textAlign="center">{category}</Typography>
+                                                </MenuItem>
+                                            ))}
+                                        </Menu>
+                                    </Box>
 
-                    <Typography
-                        variant="button"
-                        sx={{display: { xs: 'none', md: 'flex' }}}
-                    >
-                        <Link to="/contact" style={{color: 'inherit', textDecoration: 'none' }}>
-                            Contact Us
-                        </Link>
+                                    <Typography
+                                        variant="button"
+                                        sx={{display: { xs: 'none', md: 'flex' }}}
+                                    >
+                                        <Link to="/contact" style={{color: 'inherit', textDecoration: 'none' }}>
+                                            Contact Us
+                                        </Link>
+                                    </Typography>
+
+                                    <Button onClick={toggleAccordion}>
+                                        <SearchRoundedIcon/>
+                                    </Button>
+                                </Grid>
+
+                                <Grid item xs={4} sx={{display:'flex', alignItems:'center', justifyContent:'center'}}>
+                                    <Typography
+                                        variant="h6"
+                                        sx={{display:'flex'}}
+                                    >
+                                        <Link to="/" style={{color: 'inherit', textDecoration: 'none' }}>
+                                            Logo
+                                        </Link>
+                                    </Typography>
+                                </Grid>
+
+                                <Grid item xs={4} sx={{display:'flex', alignItems:'center', justifyContent:'flex-end'}}>
+                                    <IconButton sx={{display: { xs: 'none', md: 'flex' } }}>
+                                        <Link to="/profile" style={{color: 'inherit', textDecoration: 'none' }}>
+                                            <AccountIcon/>  
+                                        </Link>
+                                    </IconButton>
+
+                                    <IconButton>
+                                        <Link to="/cart" style={{color: 'inherit', textDecoration: 'none' }}>
+                                            <ShoppingCartIcon />
+                                        </Link>
+                                    </IconButton>
+                                </Grid>
+                            </Grid>
+                        </Toolbar>
+                    </Container>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Typography>
+                        Hello There!
                     </Typography>
-                    </Grid>
-
-                    <Grid item xs={4} sx={{display:'flex', alignItems:'center', justifyContent:'center'}}>
-                    <Typography
-                        variant="h6"
-                        sx={{display:'flex'}}
-                    >
-                        <Link to="/" style={{color: 'inherit', textDecoration: 'none' }}>
-                            Logo
-                        </Link>
-                    </Typography>
-                    </Grid>
-
-                    <Grid item xs={4} sx={{display:'flex', alignItems:'center', justifyContent:'flex-end'}}>
-                    <IconButton sx={{display: { xs: 'none', md: 'flex' } }}>
-                        <Link to="/profile" style={{color: 'inherit', textDecoration: 'none' }}>
-                            <AccountIcon/>  
-                        </Link>
-                    </IconButton>
-
-                    <IconButton>
-                        <Link to="/cart" style={{color: 'inherit', textDecoration: 'none' }}>
-                            <ShoppingCartIcon />
-                        </Link>
-                    </IconButton>
-                    </Grid>
-                </Grid>
-                </Toolbar>
-            </Container>
+                </AccordionDetails>
+            </Accordion>
         </AppBar>
     )
 }

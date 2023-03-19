fed42628d0ebdff89d318d463daa98f2b67624c3
Alex Ainslie <alex.ainslie99@gmail.com>
2023-03-19 18:15:46 +0000

    Fixed responsiveness issue

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Box, Container, Grid, Typography } from "@mui/material";
+import {Link} from 'react-router-dom';
+import { Box, Container, Grid, Typography, Button} from "@mui/material";
 import Logo from '../assets/footerLogo.svg';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import InstagramIcon from '@mui/icons-material/Instagram';
@@ -18,32 +19,46 @@ function Footer(){
     >
       <Container maxWidth="lg">
         <Grid container>
+
           <Grid item xs={12} sm={6} sx={{display:'flex', alignItems:'center', justifyContent:'flex-start'}}>
-            <Box>
-              <img src={Logo} alt="Logo" style={{maxHeight: "35px", background: "none"}}></img>
-              <Typography>Slogan</Typography>
-            </Box>
+            <img src={Logo} alt="Logo" style={{maxHeight: "50px", background: "none"}}></img>
+            <Typography>Slogan</Typography>
           </Grid>
+
           <Grid item xs={12} sm={6} sx={{display:'flex', alignItems:'center', justifyContent:'flex-end'}}>
             <Box>
-              <Box>
-              <a href="" target="_blank">
-                <TwitterIcon />
-              </a>
-              <a href="" target="_blank">
-                <FacebookIcon />
-              </a>
-              <a href="" target="_blank">
-                <InstagramIcon />
-              </a>
+              <Box sx={{display:'flex', justifyContent:'flex-end'}}>
+                <a href="" target="_blank">
+                  <TwitterIcon />
+                </a>
+                <a href="" target="_blank">
+                  <FacebookIcon />
+                </a>
+                <a href="" target="_blank">
+                  <InstagramIcon />
+                </a>
               </Box>
-              <Box>
-                <Typography>About</Typography>              
-                <Typography>FAQ</Typography>
-                <Typography>Contact Us</Typography>
+
+              <Box sx={{display: 'flex'}}>
+                <Button style={{color: 'inherit', textDecoration: 'none', backgroundColor: 'transparent' }}>
+                  <Link to="/" style={{color: 'inherit', textDecoration: 'none' }}>
+                    About
+                  </Link>
+                </Button>            
+                <Button style={{color: 'inherit', textDecoration: 'none', backgroundColor: 'transparent' }}>
+                  <Link to="/" style={{color: 'inherit', textDecoration: 'none' }}>
+                    FAQ
+                  </Link>
+                </Button> 
+                <Button style={{color: 'inherit', textDecoration: 'none', backgroundColor: 'transparent' }}>
+                  <Link to="/contact" style={{color: 'inherit', textDecoration: 'none' }}>
+                    Contact Us
+                  </Link>
+                </Button> 
               </Box>
             </Box>
           </Grid>
+
           <Grid item xs={12} sx={{borderTop: "1px solid darkgrey"}}>
             <Typography color="textSecondary" variant="subtitle1">
               {`© ${new Date().getFullYear()} Shop365 | All Rights Reserved`}

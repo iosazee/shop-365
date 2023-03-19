06cf3a71798c74fb3cb439c482d19ebd95df8b1a
Alex Ainslie <alex.ainslie99@gmail.com>
2023-03-19 17:46:55 +0000

    added social icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { Box, Container, Grid, Typography } from "@mui/material";
 import Logo from '../assets/footerLogo.svg';
+import TwitterIcon from '@mui/icons-material/Twitter';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import FacebookIcon from '@mui/icons-material/Facebook';
 
 function Footer(){
   return (
@@ -15,25 +18,33 @@ function Footer(){
     >
       <Container maxWidth="lg">
         <Grid container>
-          <Grid item xs={12} sm={6}>
+          <Grid item xs={12} sm={6} sx={{display:'flex', alignItems:'center', justifyContent:'flex-start'}}>
             <Box>
               <img src={Logo} alt="Logo" style={{maxHeight: "35px", background: "none"}}></img>
-            </Box>
-            <Box>
               <Typography>Slogan</Typography>
             </Box>
           </Grid>
-          <Grid item xs={12} sm={6}>
+          <Grid item xs={12} sm={6} sx={{display:'flex', alignItems:'center', justifyContent:'flex-end'}}>
             <Box>
-              <Typography>Socials</Typography>
-            </Box>
-            <Box>
-              <Typography>About</Typography>
-              <Typography>FAQ</Typography>
-              <Typography>Contact Us</Typography>
+              <Box>
+              <a href="" target="_blank">
+                <TwitterIcon />
+              </a>
+              <a href="" target="_blank">
+                <FacebookIcon />
+              </a>
+              <a href="" target="_blank">
+                <InstagramIcon />
+              </a>
+              </Box>
+              <Box>
+                <Typography>About</Typography>              
+                <Typography>FAQ</Typography>
+                <Typography>Contact Us</Typography>
+              </Box>
             </Box>
           </Grid>
-          <Grid item xs={12}>
+          <Grid item xs={12} sx={{borderTop: "1px solid darkgrey"}}>
             <Typography color="textSecondary" variant="subtitle1">
               {`© ${new Date().getFullYear()} Shop365 | All Rights Reserved`}
             </Typography>

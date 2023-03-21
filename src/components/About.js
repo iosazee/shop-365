b0e360a74365e1b6d46d6389566cd7236156350d
Alex Ainslie <alex.ainslie99@gmail.com>
2023-03-21 18:42:13 +0000

    Added spacing for about

--- src/components/About.js
+++ src/components/About.js
@@ -6,12 +6,17 @@ function About(){
     return(
         <Container>
             <Typography variant="h3" style={{marginTop:25}}>About Us</Typography>
-            <Typography>
-                Mission statement
-            </Typography>
-            <Typography>
-                At Shop 365, we believe that everyone deserves access to high-quality products that enhance their lives. We are committed to providing a wide selection of goods, including men's and women's clothing, jewelry, and electronics, at affordable prices. Our mission is to make shopping easy, convenient, and enjoyable for all of our customers, while also providing exceptional customer service and fast shipping. We strive to be a one-stop shop for all of your needs, and we are always looking for new and innovative ways to improve your shopping experience. At Shop 365, we believe that shopping should be an experience, not a chore.
-            </Typography>
+            <Container  sx={{my: 4}}>
+                <Typography sx={{my: 1}}>
+                    At Shop 365, we believe that everyone deserves access to high-quality products that enhance their lives. We are committed to providing a wide selection of goods, including men's and women's clothing, jewelry, and electronics, at affordable prices. Our mission is to make shopping easy, convenient, and enjoyable for all of our customers, while also providing exceptional customer service and fast shipping.
+                </Typography>
+                <Typography sx={{my: 1}}>
+                    We strive to be a one-stop shop for all of your needs, and we are always looking for new and innovative ways to improve your shopping experience.
+                </Typography>
+                <Typography sx={{my: 1}}>
+                    At Shop 365, we believe that shopping should be an experience, not a chore.
+                </Typography>
+            </Container>
         </Container>
     )
 }

d7c0d1b0e3570d6dd551cc299b3f49e45a5bdee0
Alex Ainslie <alex.ainslie99@gmail.com>
2023-03-21 16:59:01 +0000

    add question and fixed Typo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -51,7 +51,7 @@ function Footer(){
                 </Button>         
                 <Button style={{color: '#303030', textDecoration: 'none', backgroundColor: 'transparent' }}>
                   <Link to="/faq" style={{color: '#303030', textDecoration: 'none' }}>
-                    FAQ
+                    FAQs
                   </Link>
                 </Button> 
                 <Button style={{color: '#303030', textDecoration: 'none', backgroundColor: 'transparent' }}>

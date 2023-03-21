27e582751a404d2910fa2c64e44e363c19eef64c
Michael Davies <mike@netly.co.uk>
2023-03-21 16:41:48 +0000

    removed addshopingcart icon import

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -4,7 +4,6 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import AddShoppingCartOutlinedIcon from '@mui/icons-material/AddShoppingCartOutlined'
 import { Link } from 'react-router-dom';
 import LazyLoad from 'react-lazy-load';
 

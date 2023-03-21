4bfef9e990fb8e7694dbfc044e5704b2f74f6a2c
Alex Ainslie <alex.ainslie99@gmail.com>
2023-03-21 16:06:53 +0000

    Added mission statement

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,12 @@ function About(){
         <Container>
             <Typography>About Us</Typography>
             <Paper elevation={3} sx={{my: 3}}>
-                SHOP 365
+                <Typography>
+                    Mission statement
+                </Typography>
+                <Typography>
+                At Shop 365, we believe that everyone deserves access to high-quality products that enhance their lives. We are committed to providing a wide selection of goods, including men's and women's clothing, jewelry, and electronics, at affordable prices. Our mission is to make shopping easy, convenient, and enjoyable for all of our customers, while also providing exceptional customer service and fast shipping. We strive to be a one-stop shop for all of your needs, and we are always looking for new and innovative ways to improve your shopping experience. At Shop 365, we believe that shopping should be an experience, not a chore.
+                </Typography>
             </Paper>
         </Container>
     )
